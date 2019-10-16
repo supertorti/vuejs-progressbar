ed6283241caa26594f55effba59b36e57d90650d
supertorti <TL231298>
2019-10-16 08:59:49 +0200

    add prefix rev2

--- dist/Components/Library.js
+++ dist/Components/Library.js
@@ -4,9 +4,22 @@ const ComponentLibrary = {
 
     install (Vue) {
 
-        console.log(Components)
-    },
+        let installed = {}
 
+        for (let Component in Components){
+            let name = Components[ Component ].name
+
+            Vue.component(name, Component)
+
+            installed[ name ] = { "Message": 'Component registered'}
+        }
+
+        if(process.env.NODE_ENV === 'development') {
+            console.group('%c Install Component-Library', 'background-color: green; color: white')
+            console.table(installed)
+            console.groupEnd()
+        }
+    }
 }
 
 export default ComponentLibrary
